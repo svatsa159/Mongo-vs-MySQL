2cbf0e3cc2dbd7552b26665e3f114ca62b4de7c6
Srivatsa Rampalli <svatsa159@gmail.com>
2019-08-27 13:04:27 +0530

    Mongo Connect, JSON error

--- node/index.js
+++ node/index.js
@@ -1,8 +1,32 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+
+
+var str="";
+var express=require('express');
+var app=express();
+var c=10;
+app.get('/mov',function(req,res)
+{
+  MongoClient.connect(url, function(err, client) {
+    if (err) throw err;
+    var db=client.db('movies');
+    console.log("Database connected!");
+    var curs = db.getCollection('data').find({}).toArray;
+    // cursor.each(function(err, doc) {
+    //   if(doc!=undefined){
+    //     str=str+"&nbsp;&nbsp;&nbsp;&nbsp;Title&nbsp;&nbsp;"+doc.title+"</br>";
+    //     console.log(doc.title);}
+    // },function(err){});
+     console.log(curs);
+    res.send(curs);
+
+    client.close();
+  });
+
 });
+app.get('/',function(req,res){
+  res.send("Wasssup");
+})
+var server=app.listen(3000,function() {});
