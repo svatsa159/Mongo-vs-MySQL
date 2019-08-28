0a76e9363a132ea66b85749c3c14dd427a6f0a21
Srivatsa Rampalli <svatsa159@gmail.com>
2019-08-28 11:57:30 +0530

    EJS Render

--- node/index.js
+++ node/index.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var url = "mongodb://localhost:27017/movies";
 var express=require('express');
 var app=express();
-
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+var movie = new mongoose.Schema({
+  title: String
+},{collection:'data'});
 app.get('/',function(req,res){
   mongoose.connect(url, {useNewUrlParser: true});
   var db = mongoose.connection;
@@ -11,12 +15,15 @@ app.get('/',function(req,res){
     // we're connected!
     console.log("Connected");
   });
-  var movie = new mongoose.Schema({
-    title: String
-  },{collection:'data'});
+
+  // Schema
+
+
+//Model
   var Movie = mongoose.model('Movie', movie);
-  Movie.find().lean().exec(function (err, moviess) {
-      return res.end(JSON.stringify(moviess));
+  Movie.find({"cast":{$in:["Idris Elba"]}}).lean().exec(function (err, moviess) {
+      // return res.end(JSON.stringify(moviess));
+      return res.render("ind.ejs",{moviess:moviess,i:1})
   });
 
 });
