a932890292cd8f2e7f5ba22031fdbf089b230300
Srivatsa Rampalli <svatsa159@gmail.com>
2019-08-29 12:03:34 +0530

    Phase 2 Completed

--- client-ui/static/js/client.js
+++ client-ui/static/js/client.js
@@ -1,38 +1,70 @@
 function send(){
   var start_time = new Date().getTime();
-  document.getElementById("tt").disabled=true;
-  console.log(start_time);
-  var k=1;
+  // console.log(start_time);
+  var k=0;
+  var k1=0;
   var ele = document.getElementsByName('choice');
   for(i = 0; i < ele.length; i++) {
                 if(ele[i].checked)
                 {k=i;
-                console.log("cc");}
+                console.log(k);
+              }
             }
+            var ele1 = document.getElementsByName('choice1');
+            for(i = 0; i < ele1.length; i++) {
+                          if(ele1[i].checked)
+                          {k1=i;
+                          console.log(k1);
+                        }
+                      }
 //   jQuery.get('127.0.0.1:6099/hello',function( data,status, xhr) {
 //         var request_time = new Date().getTime() - start_time;
 //         console.log(request_time);
 // })
-if(k==0){
+if(k==0&&k1==0){
   $.ajax({
       url: "http://127.0.0.1:6069/hello",
       type: 'GET',
       success: function(res) {
         var request_time = new Date().getTime() - start_time;
                 // alert('csdc');
-                document.getElementById('name').innerHTML="Node : ";
+                document.getElementById('name').innerHTML="Node + Mongo : ";
                 document.getElementById('time').innerHTML=request_time+"ms";
                 // console.log("Node : "+request_time);
       }
   });
 }
-else if(k==1){
+else if(k==1&&k1==0){
+  $.ajax({
+      url: "http://127.0.0.1:6969/hola",
+      type: 'GET',
+      success: function(res) {
+        var request_time = new Date().getTime() - start_time;
+        document.getElementById('name').innerHTML="Python + Mongo: ";
+        document.getElementById('time').innerHTML=request_time+"ms";
+                // alert('csdc');
+      }
+  });
+}
+else if(k==0&&k1==1){
+  $.ajax({
+      url: "http://127.0.0.1:6069/hola",
+      type: 'GET',
+      success: function(res) {
+        var request_time = new Date().getTime() - start_time;
+        document.getElementById('name').innerHTML="Node + MySQL : ";
+        document.getElementById('time').innerHTML=request_time+"ms";
+                // alert('csdc');
+      }
+  });
+}
+else if(k==1&&k1==1){
   $.ajax({
       url: "http://127.0.0.1:6969/hello",
       type: 'GET',
       success: function(res) {
         var request_time = new Date().getTime() - start_time;
-        document.getElementById('name').innerHTML="Python : ";
+        document.getElementById('name').innerHTML="Python + MySQL : ";
         document.getElementById('time').innerHTML=request_time+"ms";
                 // alert('csdc');
       }
--- flask/index.py
+++ flask/index.py
@@ -11,7 +11,7 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'diksha143'
 app.config['MYSQL_DATABASE_DB'] = 'movies'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQLmongo.db._DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 conn = mysql.connect()
 cursor =conn.cursor()
@@ -28,5 +28,13 @@ def mys():
 @app.route('/hola')
 @cross_origin()
 def mong():
+    from flask import Flask
+    from flask_pymongo import PyMongo
+
+    app = Flask(__name__)
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/movies"
+    mongo = PyMongo(app)
+    data = mongo.db.data.find({}, {'_id': False})
+    return json_util.dumps(data)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6969)
